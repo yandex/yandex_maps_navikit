23381b910e1903ccd8d3e2192d5c07004d81019a
YandexMapKit <robot-mapkit-ci@yandex-team.ru>
2025-05-05 15:11:19 +0300

    Release 4.15.0-beta

--- lib/src/directions/driving/route_helper_export.dart
+++ lib/src/directions/driving/route_helper_export.dart
@@ -1,10 +1,6 @@
 export 'package:yandex_maps_navikit/src/directions/driving/route_helper.dart'
     hide
         ManeuverStyleContainerExtension,
-        PolygonManeuverStyleNative,
         ArrowManeuverStyleNative,
-        PolygonManeuverStyleContainerExtension,
-        PolygonManeuverRenderModeImpl,
-        PolygonManeuverRenderModeContainerExtension,
         ManeuverStyleNative,
         ArrowManeuverStyleContainerExtension;
