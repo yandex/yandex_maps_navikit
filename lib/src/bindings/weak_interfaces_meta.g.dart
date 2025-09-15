77a32f2f0572d11a2c82fd3e87d12c6e5b89506e
YandexMapKit <robot-mapkit-ci@yandex-team.ru>
2025-09-15 15:50:57 +0300

    Release 4.22.0-beta

--- lib/src/bindings/weak_interfaces_meta.g.dart
+++ lib/src/bindings/weak_interfaces_meta.g.dart
@@ -44,8 +44,6 @@ import 'package:yandex_maps_navikit/src/mapkit/map/placemark_presentation.dart'
     as mapkit;
 import 'package:yandex_maps_navikit/src/mapkit/map/placemark_text.dart'
     as mapkit;
-import 'package:yandex_maps_navikit/src/mapkit/map/placemark_view.dart'
-    as mapkit;
 import 'package:yandex_maps_navikit/src/mapkit/map/placemarks_styler.dart'
     as mapkit;
 import 'package:yandex_maps_navikit/src/mapkit/map/polygon.dart' as mapkit;
@@ -157,7 +155,6 @@ const weakInterfacesMeta = {
   'mapkit.map.PlacemarkPresentation':
       mapkit.PlacemarkPresentationImpl.fromNativePtrImpl,
   'mapkit.map.PlacemarkText': mapkit.PlacemarkTextImpl.fromNativePtrImpl,
-  'mapkit.map.PlacemarkView': mapkit.PlacemarkViewImpl.fromNativePtrImpl,
   'mapkit.map.PlacemarksStyler': mapkit.PlacemarksStylerImpl.fromNativePtrImpl,
   'mapkit.map.PolygonMapObject': mapkit.PolygonMapObjectImpl.fromNativePtrImpl,
   'mapkit.map.PolylineMapObject':
--- lib/src/navigation/automotive/simulation/simulation.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:core' as core;
-
-part 'simulation.impl.dart';
-
-class Simulation {
-  Simulation._();
-}
