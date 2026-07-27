1546480f466932766bddfa9079125bfb6266ef73
YandexMapKit <robot-mapkit-ci@yandex-team.ru>
2026-07-27 13:23:05 +0300

    Release 4.42.0-beta

--- lib/src/search/search_manager.dart
+++ lib/src/search/search_manager.dart
@@ -152,8 +152,9 @@ abstract class SearchManager implements ffi.Finalizable {
   /// [search_search_options.SearchOptions.resultPageSize],
   /// [search_search_options.SearchOptions.similarOrgsRequest],
   /// [search_search_options.SearchOptions.searchArea],
-  /// [search_search_options.SearchOptions.overriddenSerpId] and
-  /// [search_search_options.SearchOptions.discovery].
+  /// [search_search_options.SearchOptions.overriddenSerpId],
+  /// [search_search_options.SearchOptions.discovery] and
+  /// [search_search_options.SearchOptions.toponymDiscoveryGeoId].
   /// [searchListener] Listener to handle search result.
   ///
   /// Return [search_session.SearchSession] which allows search cancel and
